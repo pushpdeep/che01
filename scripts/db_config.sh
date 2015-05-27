2708690f852ccdfc9ca49e953cb2c7b03c1866f3
pushpdeep <pushpdeepmishra@gmail.com>
2015-05-27 12:47:35 +0530

    added db

--- scripts/db_config.sh
+++ scripts/db_config.sh
@@ -5,13 +5,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install mysql-server
 
 # mysql -u root -Bse "create database YOUR_DATABASE_NAME;"
-mysql -u root -Bse "create database che01;"
+mysql -u root -Bse "create database cell;"
 
 cd ../src/database
 
 # Replace "che01"  with your database name 
-# Replace "che01.sql" file with your file
+# Replace "cell.sql" file with your file
 # Note: If you have more than one .sql file repeat below command w.r.t to database and file name
 
-mysql -u root che01<che01.sql
+mysql -u root cell<cell.sql
 
